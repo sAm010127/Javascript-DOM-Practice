b0d8bd53d00a32b70c98129229f29be17fd5e57f
Khamoosh Sam <sam.kiani101@gmail.com>
2025-09-22 09:11:49 +0430

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,60 +1,76 @@
 let name = document.getElementById('name');
-let lastName = document.getElementById('lname');
+let lastName = document.getElementById('lName');
 let date = document.getElementById('date');
 let myTable = document.getElementById('tbd');
 let submitBtn = document.getElementById('submit');
+let searchBtn = document.getElementById('search');
 
-let employees = [];
-let mainIndex = -1;
-
-const makeTable = ()=>  {
-    myTable.innerHTML = employees.map((emp, i) => {
-        return `
+let id = 0;
+let employees = JSON.parse(localStorage.getItem('myList')?? "[]");
+const makeTable = (myList = employees)=>  {
+    myTable.innerHTML = myList.map((emp, i) => {
+        return  `
           <tr data-index="${i}">
             <td>${i + 1}</td>
             <td>${(emp.name)}</td>
             <td>${(emp.lastName)}</td>
             <td>${(emp.date)}</td>
             <td class="actions">
-              <button class="edit"  onclick="editRow(${i})">Edit</button>
-              <button class="edit delete" onclick="destroy(${i})">Delete</button>
+              <button class="edit"  onclick="editRow(${emp.id})">Edit</button>
+              <button class="edit delete" onclick="destroy(${emp.id})">Delete</button>
             </td>
           </tr>
         `;
     }).join('');
+
 }
+makeTable();
 
 const store = ()=>{
-    if(mainIndex === -1){
-        employees.push({name : name.value, lastName : lastName.value, date : date.value});
+    if(submitBtn.textContent === 'Save'){
+        id = new Date().getTime();
+        employees.push({name : name.value, lastName : lastName.value, date : date.value, id});
     }
     else{
-        employees[mainIndex]= {name : name.value, lastName : lastName.value, date : date.value};
+        let index = employees.findIndex((e) => e.id === id);
+        employees[index]= {name : name.value, lastName : lastName.value, date : date.value};
     }
+    localStorage.setItem("myList", JSON.stringify(employees))
     resetForm();
     makeTable();
-    updated();
 }
 const resetForm = ()=>{
     name.value = '';
     lastName.value = '';
     date.value = '';
+    submitBtn.textContent = 'Save';
 }
-const editRow = (rowIndex) => {
-    mainIndex = rowIndex;
-    name.value = employees[rowIndex].name;
-    lastName.value = employees[rowIndex].lastName;
-    date.value = employees[rowIndex].date;
+const editRow = (row_id) => {
+    let element = employees.find((e) => e.id === row_id);
+    name.value = element.name;
+    lastName.value = element.lastName;
+    date.value = element.date;
 
     submitBtn.textContent = 'Update';
     name.focus();
 }
-const updated = ()=>{
-    mainIndex = -1;
-    submitBtn.textContent = 'Save';
+
+const destroy = (row_id)=> {
+    if (confirm('Are you sure you want to delete this row?')) {
+        let index = employees.findIndex((e) => e.id === id);
+        employees.splice(index, 1);
+        localStorage.setItem("myList", JSON.stringify(employees));
+        makeTable();
+    }
 }
-const destroy = (rowIndex)=>{
-    if(!confirm('Are you sure you want to delete this row?')) return;
-    employees.splice(rowIndex, 1);
-    makeTable();
-}
+
+searchBtn.addEventListener('input', () => {
+    const inputValue = searchBtn.value.toLowerCase();
+
+    const filtered = employees.filter(emp =>
+        emp.name.toLowerCase().includes(inputValue) ||
+        emp.lastName.toLowerCase().includes(inputValue)
+    );
+    makeTable(filtered);
+});
+
